49866880666cb37255fa6921ffeee903d6e804eb
25LP0902 25LP0902 <25LP0902@admins-iMac-2.local>
2025-12-25 19:04:15 +0800

    deleted unused thing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
-import prisma from "../lib/prisma";
 import { HomeScreen } from "./_features/homeScreen";
 
 export default async function Home() {
-  const users = await prisma.user.findMany();
-  console.log(users, "idk");
-
   return (
     <>
       <HomeScreen />
