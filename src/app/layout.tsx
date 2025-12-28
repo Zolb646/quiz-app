cb76cfab4b6094a83c2616958cd4b175c296c587
Zolb646 <zolb646@gmail.com>
2025-12-28 22:48:57 +0800

    checking things

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default function RootLayout({
               <body
                 className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
               >
-                <InitUser />
+                {/* <InitUser /> */}
                 {children}
                 <ToastContainer
                   position="top-center"
